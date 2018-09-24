4932c83dd6b3aa28b920b7b79986169df3e8170a
212223Matrixboy <dei.cerniauskas@gmail.com>
2018-09-24 13:51:49 +0300

    init commit

---
+++ src/com/company/RenderPanel.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class RenderPanel extends JPanel {
+
+    public static int curColor = 0;
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(new Color (curColor));
+        g.fillRect(0,0, 800,700);
+        curColor++;
+    }
+}
